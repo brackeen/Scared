4494f07160b52a4f1ca53b7bb13bd818abf70178
David Brackeen <brackeen@gmail.com>
2016-04-16 16:55:04 -0700

    Add method for multiline labels

--- src/main/java/com/brackeen/app/view/Label.java
+++ src/main/java/com/brackeen/app/view/Label.java
@@ -51,4 +51,31 @@ public class Label extends View {
             font.drawString(g, text);
         }
     }
+    
+    public static View makeMultilineLabel(BitmapFont font, String text, float anchorX) {
+        View view = new View();
+        int y = 0;
+        int index = 0;
+        while (true) {
+            String line;
+            int newIndex = text.indexOf('\n', index);
+            if (newIndex == -1) {
+                line = text.substring(index);
+            } else {
+                line = text.substring(index, newIndex);
+            }
+            Label label = new Label(font, line);
+            label.setLocation(0, y);
+            label.setAnchor(anchorX, 0);
+            view.addSubview(label);
+            y += font.getHeight();
+            
+            if (newIndex == -1) {
+                break;
+            }
+            index = newIndex + 1;
+        }
+        view.setHeight(y);
+        return view;
+    }
 }
